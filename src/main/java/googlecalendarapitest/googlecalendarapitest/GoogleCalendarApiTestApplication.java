459c8be4f57a03ae062ed037e059c68caaff62b2
SsangSoo <kss10080@naver.com>
2024-07-05 23:47:17 +0900

    OK

---
+++ src/main/java/googlecalendarapitest/googlecalendarapitest/GoogleCalendarApiTestApplication.java
@@ -0,0 +1,13 @@
+package googlecalendarapitest.googlecalendarapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleCalendarApiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/googlecalendarapitest/googlecalendarapitest/GoogleCalendarApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package googlecalendarapitest.googlecalendarapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCalendarApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
